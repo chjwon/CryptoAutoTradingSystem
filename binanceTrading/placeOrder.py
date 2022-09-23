f8218c0a624752433e63480362168108eb24de0d
Jae Won Choi <chjwon10044@gmail.com>
2022-09-22 19:54:41 -0500

    little change

--- binanceTrading/placeOrder.py
+++ binanceTrading/placeOrder.py
@@ -3,20 +3,20 @@ import ccxt
 binance = ccxt.binance()
 
 
-def orderMarketPrice(coinTicker, buy_sell, amount):
-    if buy_sell == "buy":
+def orderMarketPrice(coinTicker, position, amount):
+    if position == "buy":
         order = binance.create_limit_buy_order(coinTicker, amount)
-    elif buy_sell == "sell":
+    elif position == "sell":
         order = binance.create_limit_sell_order(coinTicker, amount)
     else:
         pass
     return str(order)
 
 
-def orderLimitPrice(coinTicker, buy_sell, amount, price):
-    if buy_sell == "buy":
+def orderLimitPrice(coinTicker, position, amount, price):
+    if position == "buy":
         order = binance.create_limit_buy_order(coinTicker, amount, price)
-    elif buy_sell == "sell":
+    elif position == "sell":
         order = binance.create_limit_sell_order(coinTicker, amount, price)
     else:
         pass
